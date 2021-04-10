d1b04ae586aa6afc23ab410ede7cb7e250b1faf8
Izlaster <izmaylovle0@gmail.com>
2021-04-10 19:15:54 +0300

    final commit

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -7,11 +7,11 @@ contract MyContract
     address player2;
     uint public numb1;
     uint public numb2;
-    uint public rand = 5;
+    uint public rand;
     uint winner;
 
-    // event GameEvent(address player);
-    // event WinEvent(int winnerNumber);
+    event WhoPushed(address player);
+    event ShowRandomVal(uint random);
     
     modifier isRegistered() {
         require(msg.sender != player1 && msg.sender != player2);
@@ -21,6 +21,10 @@ contract MyContract
     modifier isBothReg() {
         require((int(player1) != 0) && (int(player2) != 0));
         _;
+    }
+
+    function Random() public view returns(uint) { //Мы не знаем как это работает, но это рандомайзер
+        return uint(keccak256(abi.encodePacked(now, block.difficulty, msg.sender))) % 10;
     }
 
     function RegMe() isRegistered() public {
@@ -33,12 +37,16 @@ contract MyContract
     function TryNumber(uint number) isBothReg() public {
         if (msg.sender == player1) {
             numb1 = number;
+            emit WhoPushed(player1);
             count++;
         } else if (msg.sender == player2) {
             numb2 = number;
+            emit WhoPushed(player2);
             count++;
         }
         if (count == 2) {
+            rand = Random();
+            emit ShowRandomVal(rand);
             DeadermineTheWinner();
         }
     }
@@ -72,12 +80,11 @@ contract MyContract
         delete numb1;
         delete numb2;
         winner = 0;
-
     }
 
-    function GetRez() public view returns (uint w1nn3r) {
-        w1nn3r = winner;
-        ClearAllData();   
+    function GetRez() public view returns (uint w1nn3r, uint random) {
+        w1nn3r = winner;  
+        random = rand;
     }
 
     function WhoAmI() public view returns (uint number) {
