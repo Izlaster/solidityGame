c13fe7993438bf954df925d10a7c45e1bc69c7e2
Izlaster <izmaylovle0@gmail.com>
2021-06-07 14:47:40 +0300

    final final commit

--- migrations/2_initial_mycontract.js
+++
@@ -1,9 +0,0 @@
-const MyContract = artifacts.require("MyContract");
-const TokenOBT = artifacts.require("TokenOBT");
-
-module.exports = function(deployer) {
-  deployer.deploy(TokenOBT)
-    .then((Token) => {
-      return deployer.deploy(MyContract, Token.address);
-    });
-};
