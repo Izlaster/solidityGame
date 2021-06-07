797fdccdbc6b4cd0fa203b645740f36770b47534
Izlaster <izmaylovle0@gmail.com>
2021-06-07 14:11:57 +0300

    another finale commit

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,7 +1,9 @@
 pragma solidity >=0.4.22 <0.9.0;
+import "./TokenOBT.sol";
 
 contract MyContract
 {
+    TokenOBT public Token;
     uint count = 0;
     address player1;
     address player2;
@@ -9,9 +11,16 @@ contract MyContract
     uint public numb2;
     uint public rand;
     uint winner;
+    uint256 BID = uint256(100);
 
     event WhoPushed(address player);
     event ShowRandomVal(uint random);
+
+    constructor(address tokenAddr) public
+    {
+        Token = TokenOBT(tokenAddr);
+    }
+
     
     modifier isRegistered() {
         require(msg.sender != player1 && msg.sender != player2);
@@ -23,15 +32,19 @@ contract MyContract
         _;
     }
 
-    function Random() public view returns(uint) { //Мы не знаем как это работает, но это рандомайзер
+    function Random() public view returns(uint) { //Мы не знаем как это работает, но это рандомайзер и это работает
         return uint(keccak256(abi.encodePacked(now, block.difficulty, msg.sender))) % 10;
     }
 
     function RegMe() isRegistered() public {
-        if (int(player1) == 0)
+        if (int(player1) == 0) {
             player1 = msg.sender;
-        else if (int(player2) == 0)
+            Token.transferFrom(msg.sender, address(this), BID);
+        }
+        else if (int(player2) == 0) {
             player2 = msg.sender;
+            Token.transferFrom(msg.sender, address(this), BID);
+        }
     }
 
     function TryNumber(uint number) isBothReg() public {
@@ -65,13 +78,17 @@ contract MyContract
 
         if (numb1 > numb2) {
             winner = 2;
+            Token.transfer(player2, BID * 2);
         } else if (numb1 < numb2) {
             winner = 1;
+            Token.transfer(player1, BID * 2);
         } else 
         {
             winner = 0;
+            Token.transfer(player1, BID);
+            Token.transfer(player2, BID);
         }
-    }
+    } 
 
     function ClearAllData() public {
         count = 0;
@@ -82,8 +99,11 @@ contract MyContract
         winner = 0;
     }
 
-    function GetRez() public view returns (uint w1nn3r, uint random) {
-        w1nn3r = winner;  
+    function GetRez() public view returns (uint w1nn3r) {
+        w1nn3r = winner; 
+    }
+
+     function GetRandom() public view returns (uint random) {  
         random = rand;
     }
 
